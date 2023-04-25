a81d1ea5fe4dbc9a70a7fe06ae8d60095ad15c15
Lucas Arcanjo <lucas.arcanjo@ibm.com>
2023-04-25 16:41:52 -0300

    other ways solve the problem

--- treinos/treino_1.js
+++ treinos/treino_1.js
@@ -1,35 +1,51 @@
-function somar(n1) {
-    return function (n2) {
-        return function (n3) {
-            return n1 + n2 + n3
+// function somar(n1) {
+//     return function (n2) {
+//         return function (n3) {
+//             return n1 + n2 + n3
+//         }
+//     }
+// }
+
+// function subtrair(n1) {
+//     return function (n2) {
+//         return function (n3) {
+//             return n1 - n2 - n3
+//         }
+//     }
+// }
+
+// function multiplicar(n1) {
+//     return function (n2) {
+//         return function (n3) {
+//             return n1 * n2 * n3
+//         }
+//     }
+// }
+
+// // console.log(multiplicar(10)(11)(2))
+
+// function calcular(n1, n2, fn) {
+//     if (typeof fn === 'function') {
+//         return fn(n1)(n2)
+//     }
+// }
+
+// console.log(calcular(5, 2, somar(10)))
+// console.log(calcular(5, 2, subtrair(10)))
+// console.log(calcular(5, 2, multiplicar(10)))
+
+function calcular2(a) {
+    return function(b) {
+        return function(fn) {
+            return fn(a)(b)
         }
     }
 }
 
-function subtrair(n1) {
-    return function (n2) {
-        return function (n3) {
-            return n1 - n2 - n3
-        }
+function somar2(a) {
+    return function (b) {
+        return a + b
     }
 }
 
-function multiplicar(n1) {
-    return function (n2) {
-        return function (n3) {
-            return n1 * n2 * n3
-        }
-    }
-}
-
-// console.log(multiplicar(10)(11)(2))
-
-function calcular(n1, n2, fn) {
-    if (typeof fn === 'function') {
-        return fn(n1)(n2)
-    }
-}
-
-console.log(calcular(5, 2, somar(10)))
-console.log(calcular(5, 2, subtrair(10)))
-console.log(calcular(5, 2, multiplicar(10)))
+console.log(calcular2(2)(12)(somar2))
